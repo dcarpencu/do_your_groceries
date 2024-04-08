114fdc3a8955834abc3629e4fa8ae19677eab0d4
dcarpencu <dcarpencu@gmail.com>
2024-04-08 18:51:12 +0300

    New login working properly

--- lib/src/ui_elements/components/custom_sign_in.dart
+++ lib/src/ui_elements/components/custom_sign_in.dart
@@ -2,113 +2,110 @@ import 'package:do_you_groceries/src/ui_elements/components/sign_in_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-Future<Object?> customSigninDialog(BuildContext context,
-    {required ValueChanged onClosed}) {
+Future<Object?> customSigninDialog(BuildContext context, {required ValueChanged onClosed}) {
   return showGeneralDialog(
-      barrierDismissible: true,
-      barrierLabel: "Sign up",
-      context: context,
-      transitionDuration: const Duration(milliseconds: 400),
-      transitionBuilder: (context, animation, secondaryAnimation, child) {
-        Tween<Offset> tween = Tween(begin: Offset(0, -1), end: Offset.zero);
-        return SlideTransition(
-            position: tween.animate(
-                CurvedAnimation(parent: animation, curve: Curves.easeInOut)),
-            child: child);
-      },
-      pageBuilder: (BuildContext context, _, __) => Center(
-            child: Container(
-              height: 620,
-              margin: const EdgeInsets.symmetric(horizontal: 16),
-              padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-              decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.95),
-                  borderRadius: const BorderRadius.all(Radius.circular(40))),
-              child: Scaffold(
-                backgroundColor: Colors.transparent,
-                resizeToAvoidBottomInset:
-                    false, // avoid overflow error when keyboard shows up
-                body: Stack(
-                  clipBehavior: Clip.none,
+    barrierDismissible: true,
+    barrierLabel: "Sign up",
+    context: context,
+    transitionDuration: const Duration(milliseconds: 400),
+    transitionBuilder:
+        (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+      final Tween<Offset> tween = Tween(begin: const Offset(0, -1), end: Offset.zero);
+      return SlideTransition(
+          position: tween.animate(CurvedAnimation(parent: animation, curve: Curves.easeInOut)), child: child);
+    },
+    pageBuilder: (BuildContext context, _, __) => Center(
+      child: Container(
+        height: 620,
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+        decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.95), borderRadius: const BorderRadius.all(Radius.circular(40))),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          resizeToAvoidBottomInset: false, // avoid overflow error when keyboard shows up
+          body: Stack(
+            clipBehavior: Clip.none,
+            children: <Widget>[
+              Column(children: <Widget>[
+                const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 20, fontFamily: "Poppins"),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    "Welcome! Please sign up in order to get access to our smart shopping ecosystem",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SignInForm(),
+                const Row(
                   children: [
-                    Column(children: [
-                      const Text(
-                        "Sign In",
-                        style: TextStyle(fontSize: 34, fontFamily: "Poppins"),
+                    Expanded(
+                      child: Divider(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Text(
+                        "OR",
+                        style: TextStyle(color: Colors.black26),
                       ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 16),
-                        child: Text(
-                          "Access to 240+ hours of content. Learn design and code, by builder real apps with Flutter and Swift.",
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      const SignInForm(),
-                      const Row(
-                        children: [
-                          Expanded(
-                            child: Divider(),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            child: Text(
-                              "OR",
-                              style: TextStyle(color: Colors.black26),
-                            ),
-                          ),
-                          Expanded(
-                            child: Divider(),
-                          ),
-                        ],
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20.0),
-                        child: Text("Sign up with Email, Apple or Google",
-                            style: TextStyle(color: Colors.black54)),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {},
-                              icon: SvgPicture.asset(
-                                "assets/icons/email_box.svg",
-                                height: 64,
-                                width: 64,
-                              ),),
-                          IconButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {},
-                              icon: SvgPicture.asset(
-                                "assets/icons/apple_box.svg",
-                                height: 64,
-                                width: 64,
-                              )),
-                          IconButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {},
-                              icon: SvgPicture.asset(
-                                "assets/icons/google_box.svg",
-                                height: 64,
-                                width: 64,
-                              ))
-                        ],
-                      )
-                    ]),
-                    const Positioned(
-                      left: 0,
-                      right: 0,
-                      bottom: -48,
-                      child: CircleAvatar(
-                        radius: 16,
-                        backgroundColor: Colors.white,
-                        child: Icon(Icons.close, color: Colors.black),
-                      ),
-                    )
+                    ),
+                    Expanded(
+                      child: Divider(),
+                    ),
                   ],
                 ),
-              ),
-            ),
-          )).then(onClosed);
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: Text("Sign up with Email, Apple or Google", style: TextStyle(color: Colors.black54)),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        "assets/icons/email_box.svg",
+                        height: 64,
+                        width: 64,
+                      ),
+                    ),
+                    IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/apple_box.svg",
+                          height: 64,
+                          width: 64,
+                        )),
+                    IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/icons/google_box.svg",
+                          height: 64,
+                          width: 64,
+                        ))
+                  ],
+                )
+              ]),
+              const Positioned(
+                left: 0,
+                right: 0,
+                bottom: -48,
+                child: CircleAvatar(
+                  radius: 16,
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.close, color: Colors.black),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    ),
+  ).then(onClosed);
 }
--- lib/src/ui_elements/components/sign_in_form.dart
+++ lib/src/ui_elements/components/sign_in_form.dart
@@ -46,7 +46,6 @@ class _SignInFormState extends State<SignInForm> {
     }
   }
 
-
   bool isShowLoading = false;
   bool isShowConfetti = false;
 
@@ -57,8 +56,7 @@ class _SignInFormState extends State<SignInForm> {
   late SMITrigger confetti;
 
   StateMachineController getRiveController(Artboard artboard) {
-    final StateMachineController? controller =
-        StateMachineController.fromArtboard(artboard, 'State Machine 1');
+    final StateMachineController? controller = StateMachineController.fromArtboard(artboard, 'State Machine 1');
     artboard.addController(controller!);
     return controller;
   }
@@ -95,114 +93,119 @@ class _SignInFormState extends State<SignInForm> {
       children: <Widget>[
         Form(
             key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Text(
-                  'Email',
-                  style: TextStyle(color: Colors.black54),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8, bottom: 16),
-                  child: TextFormField(
-                    controller: _email,
-                    keyboardType: TextInputType.emailAddress,
-                    textInputAction: TextInputAction.next,
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        prefixIcon: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: SvgPicture.asset("assets/icons/email.svg"),
-                        )),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'please provide an email';
-                      } else if (!value.contains('@')) {
-                        return 'please enter a valid email';
-                      }
-                      return null;
-                    },
-                    onFieldSubmitted: (String value) {
-                      FocusScope.of(context).requestFocus(_passwordNode);
-                    },
-                  )
-                ),
-                const Text(
-                  "Password",
-                  style: TextStyle(color: Colors.black54),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, bottom: 16),
-                  child:                       TextFormField(
-                    controller: _password,
-                    focusNode: _passwordNode,
-                    textInputAction: TextInputAction.done,
-                    obscureText: true,
-                      decoration: InputDecoration(
-                          prefixIcon: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: SvgPicture.asset("assets/icons/password.svg"),
-                          )),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'please provide a valid password';
-                      } else if (value.length < 6) {
-                        return 'Please provide a password longer than 6 characters';
-                      }
-                      return null;
-                    },
-                    onFieldSubmitted: (String value) {
-                      _onNext(context);
-                    },
-                  )
+            child: Builder(builder: (BuildContext context) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'Email',
+                    style: TextStyle(color: Colors.black54),
                   ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, bottom: 24),
-                  child: ElevatedButton.icon(
-                      onPressed: () {
-                        signIn(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFFF77D8E),
-                          minimumSize: const Size(double.infinity, 56),
-                          shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(25),
-                                  bottomRight: Radius.circular(25),
-                                  bottomLeft: Radius.circular(25)))),
-                      icon: const Icon(
-                        CupertinoIcons.arrow_right,
-                        color: Color(0xFFFE0037),
-                      ),
-                      label: const Text("Sign In")),
-                )
-              ],
-            )),
-        if (isShowLoading) CustomPositioned(
-                child: RiveAnimation.asset(
-                "assets/RiveAssets/check.riv",
-                onInit: (artboard) {
-                  StateMachineController controller =
-                      getRiveController(artboard);
-                  check = controller.findSMI("Check") as SMITrigger;
-                  error = controller.findSMI("Error") as SMITrigger;
-                  reset = controller.findSMI("Reset") as SMITrigger;
-                },
-              )) else const SizedBox(),
-        if (isShowConfetti) CustomPositioned(
-                child: Transform.scale(
-                scale: 6,
-                child: RiveAnimation.asset(
-                  "assets/RiveAssets/confetti.riv",
-                  onInit: (artboard) {
-                    StateMachineController controller =
-                        getRiveController(artboard);
-                    confetti =
-                        controller.findSMI("Trigger explosion") as SMITrigger;
-                  },
-                ),
-              )) else const SizedBox()
+                  Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 16),
+                      child: TextFormField(
+                        controller: _email,
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        autofocus: false,
+                        decoration: InputDecoration(
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: SvgPicture.asset("assets/icons/email.svg"),
+                            )),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'please provide an email';
+                          } else if (!value.contains('@')) {
+                            return 'please enter a valid email';
+                          }
+                          return null;
+                        },
+                        onFieldSubmitted: (String value) {
+                          FocusScope.of(context).requestFocus(_passwordNode);
+                        },
+                      )),
+                  const Text(
+                    "Password",
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(top: 8.0, bottom: 16),
+                      child: TextFormField(
+                        controller: _password,
+                        focusNode: _passwordNode,
+                        textInputAction: TextInputAction.done,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: SvgPicture.asset("assets/icons/password.svg"),
+                            )),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'please provide a valid password';
+                          } else if (value.length < 6) {
+                            return 'Please provide a password longer than 6 characters';
+                          }
+                          return null;
+                        },
+                        onFieldSubmitted: (String value) {
+                          _onNext(context);
+                        },
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 24),
+                    child: ElevatedButton.icon(
+                        onPressed: () {
+                          signIn(context);
+                          _onNext(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFF77D8E),
+                            minimumSize: const Size(double.infinity, 56),
+                            shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    topRight: Radius.circular(25),
+                                    bottomRight: Radius.circular(25),
+                                    bottomLeft: Radius.circular(25)))),
+                        icon: const Icon(
+                          CupertinoIcons.arrow_right,
+                          color: Color(0xFFFE0037),
+                        ),
+                        label: const Text("Sign In")),
+                  )
+                ],
+              );
+            },)
+        ),
+        if (isShowLoading)
+          CustomPositioned(
+              child: RiveAnimation.asset(
+            "assets/RiveAssets/check.riv",
+            onInit: (Artboard artboard) {
+              final StateMachineController controller = getRiveController(artboard);
+              check = controller.findSMI("Check") as SMITrigger;
+              error = controller.findSMI("Error") as SMITrigger;
+              reset = controller.findSMI("Reset") as SMITrigger;
+            },
+          ))
+        else
+          const SizedBox(),
+        if (isShowConfetti)
+          CustomPositioned(
+              child: Transform.scale(
+            scale: 6,
+            child: RiveAnimation.asset(
+              "assets/RiveAssets/confetti.riv",
+              onInit: (artboard) {
+                final StateMachineController controller = getRiveController(artboard);
+                confetti = controller.findSMI("Trigger explosion") as SMITrigger;
+              },
+            ),
+          ))
+        else
+          const SizedBox()
       ],
     );
   }
@@ -210,6 +213,7 @@ class _SignInFormState extends State<SignInForm> {
 
 class CustomPositioned extends StatelessWidget {
   const CustomPositioned({super.key, required this.child, this.size = 100});
+
   final Widget child;
   final double size;
 
@@ -217,14 +221,14 @@ class CustomPositioned extends StatelessWidget {
   Widget build(BuildContext context) {
     return Positioned.fill(
       child: Column(
-        children: [
-          Spacer(),
+        children: <Widget>[
+          const Spacer(),
           SizedBox(
             height: size,
             width: size,
             child: child,
           ),
-          Spacer(flex: 2),
+          const Spacer(flex: 2),
         ],
       ),
     );
